71f1158da49ae89b44d52bafde4734f403b38675
Heston Liebowitz <hestonl@gmail.com>
2026-04-01 11:53:57 -0600

    Use outline button for cancel in dialog

--- LabelMaker/app/src/main/java/com/hestonliebowitz/labelmaker/MainActivity.kt
+++ LabelMaker/app/src/main/java/com/hestonliebowitz/labelmaker/MainActivity.kt
@@ -42,6 +42,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
@@ -369,7 +370,7 @@ fun Settings(
                             }
                         },
                         dismissButton = {
-                            Button(onClick = { showDialog = false }) {
+                            OutlinedButton(onClick = { showDialog = false }) {
                                 Text(stringResource(id = R.string.cancel))
                             }
                         }
